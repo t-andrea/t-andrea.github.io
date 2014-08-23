bfb61ee77f9f2d38c1a723ad3ef14a96ec270280
analdana <analdana@ncsu.edu>
2014-08-23 19:59:54 -0400

    Edit app.js function
    Add functionality to menu button so that if you click it while the menu
    is open it will close it.

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 // JavaScript Document
+var isOpen = false;
+
 var main = function() {
   /* Push the body and the nav over by 285px over */
+   
   $('.icon-menu').click(function() {
-    $('.menu').animate({
+	  if( isOpen == false ) {
+		  openMenu;
+		  isOpen = true;
+	  } else {
+		  closeMenu;
+		  isOpen = false;
+	  }
+    
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    closeMenu;
+  });
+};
+
+var openMenu = function() {
+	$('.menu').animate({
       left: "0px"
     }, 200);
 
     $('body').animate({
       left: "285px"
     }, 200);
-  });
-
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
-
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-};
+}
+var closeMenu = function() {
+	$('.menu').animate({
+		left: "-285px"
+	}, 200);
+	
+	$('body').animate({
+		left: "0px"
+	}, 200);
+}
 
 
 $(document).ready(main);
